5b2e2d7c08c587afb7b1a30a67a08f098f0c4b9a
Anders Ryden <rydena021@gmail.com>
2017-02-16 13:33:15 +0100

    add views for cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,4 @@
 class CocktailsController < ApplicationController
-  before_action :set_cocktail, only: [ :show, :edit, :update, :destroy ]
 
   def index
     @cocktails = Cocktail.all
@@ -7,6 +6,7 @@ class CocktailsController < ApplicationController
 
 
   def show
+      @cocktail = Cocktail.find(params[:id])
   end
 
 
@@ -24,33 +24,9 @@ class CocktailsController < ApplicationController
     end
   end
 
-
-  def edit
-  end
-
-
-  def update
-    if @cocktail.update(cocktail_params)
-      redirect_to cocktail_path(@cocktail)
-    else
-      render :new
-    end
-  end
-
-
-  def destroy
-    @cocktail.destroy
-    redirect_to cocktails_path
-  end
-
-
   private
 
-  def set_cocktail
-    @cocktail = Cocktail.find(params[:id])
-  end
-
   def cocktail_params
-    params.require(:cocktail).permit(:name, :address, :category, :phone_number)
+    params.require(:cocktail).permit(:name)
   end
 end
